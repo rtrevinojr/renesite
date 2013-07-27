00b7a6f01aadd666d20694b48431b8b858c2a501
Rene  Trevino Jr <renetrevinojr@Renes-MacBook-Pro.local>
2013-07-26 23:58:01 -0500

    heroku manage.py

--- manage.py
+++ manage.py
@@ -23,7 +23,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cache_money.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rene.settings")
 
     from django.core.management import execute_from_command_line
 
